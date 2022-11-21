b483c43aa99ae7ab9db5e495e056759fb064fac5
Madhavan Raja <madhavanraja99@gmail.com>
2022-11-20 17:10:15 -0700

    Rectangles are now yellow

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,4 +1,5 @@
 import java.awt.Graphics;
+import java.awt.Color;
 
 import javax.swing.JPanel;
 
@@ -12,6 +13,9 @@ public class Rectangle implements DrawRectangleInterface {
 
 	@Override
 	public void draw(int x, int y, String name) {
+		g.setColor(Color.YELLOW);
+		g.fillRect(x - Config.BOX_WIDTH / 2, y - Config.BOX_HEIGHT / 2, Config.BOX_WIDTH, Config.BOX_HEIGHT);
+		g.setColor(Color.BLACK);
 		g.drawRect(x - Config.BOX_WIDTH / 2, y - Config.BOX_HEIGHT / 2, Config.BOX_WIDTH, Config.BOX_HEIGHT);
 		g.drawString(name, x - Config.BOX_WIDTH / 2 + Config.BOX_TEXT_OFFSET, y);
 	}
